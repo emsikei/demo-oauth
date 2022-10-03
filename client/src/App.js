479924d9b88408e983c1deecb629f69e21900ff6
Andrey Gagauz <gagauzandrey24@gmail.com>
2022-10-03 10:20:56 +0300

    Facebook auth

--- client/src/App.js
+++ client/src/App.js
@@ -1,12 +1,21 @@
 function App() {
-  const url = `https://accounts.google.com/o/oauth2/v2/auth?scope=${encodeURIComponent('https://www.googleapis.com/auth/userinfo.profile')}&access_type=offline&include_granted_scopes=true&response_type=code&redirect_uri=${encodeURIComponent('http://localhost:8080/google')}&client_id=72077841259-naqli9572qehlso0cfclkefndk7lgj3s.apps.googleusercontent.com`
+  const FACEBOOK_CLIENT_ID = "1246606399427378";
+  const FACEBOOK_CLIENT_SECRET = "2b4b5cb882c060ef1f70aac44b324e81";
+  
+  const GOOGLE_URL = `https://accounts.google.com/o/oauth2/v2/auth?scope=${encodeURIComponent('https://www.googleapis.com/auth/userinfo.profile')}&access_type=offline&include_granted_scopes=true&response_type=code&redirect_uri=${encodeURIComponent('http://localhost:8080/google')}&client_id=72077841259-naqli9572qehlso0cfclkefndk7lgj3s.apps.googleusercontent.com`
+  
+  const FACEBOOK_URL = `https://www.facebook.com/v15.0/dialog/oauth?client_id=${FACEBOOK_CLIENT_ID}&redirect_uri=${encodeURIComponent('http://localhost:8080/facebook')}&state={st=state123abc,ds=123456789}&scope=public_profile,email&auth_type=rerequest`
 
-  console.log(url)
+
+  console.log(FACEBOOK_URL)
   
   return (
     <div className="App">
       <button>
-        <a href={url}>Google</a>
+        <a href={GOOGLE_URL}>Google</a>
+      </button>
+      <button>
+        <a href={FACEBOOK_URL}>Facebook</a>
       </button>
     </div>
   );
--- server/index.js
+++ server/index.js
@@ -1,8 +1,11 @@
 import express, { response } from "express";
 import axios from "axios";
 
-const CLIENT_SECRET = "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";
-const CLIENT_ID = "72077841259-naqli9572qehlso0cfclkefndk7lgj3s.apps.googleusercontent.com"
+const GOOGLE_CLIENT_SECRET = "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";
+const GOOGLE_CLIENT_ID = "72077841259-naqli9572qehlso0cfclkefndk7lgj3s.apps.googleusercontent.com"
+
+const FACEBOOK_CLIENT_ID = "1246606399427378";
+const FACEBOOK_CLIENT_SECRET = "2b4b5cb882c060ef1f70aac44b324e81";
 
 const app = express();
 
@@ -12,7 +15,7 @@ app.get('/google', async (req, res) => {
 
     // https://developers.google.com/google-ads/api/docs/best-practices/common-errors
     try {
-        const url = `https://www.googleapis.com/oauth2/v3/token?code=${code}&client_id=${CLIENT_ID}&client_secret=${CLIENT_SECRET}&grant_type=authorization_code&redirect_uri=${encodeURIComponent('http://localhost:8080/google')}`;
+        const url = `https://www.googleapis.com/oauth2/v3/token?code=${code}&client_id=${GOOGLE_CLIENT_ID}&client_secret=${GOOGLE_CLIENT_SECRET}&grant_type=authorization_code&redirect_uri=${encodeURIComponent('http://localhost:8080/google')}`;
         
         const response = await axios.post(url) 
 
@@ -25,6 +28,47 @@ app.get('/google', async (req, res) => {
     } catch (error) {
         console.log(error.response)
     }
+})
+
+app.get('/facebook', async (req, res) => {
+    const {code} = req.query;
+
+    console.log(code);
+
+    try {
+        const response = await axios.post(
+            "https://graph.facebook.com/v15.0/oauth/access_token",
+            {},
+            {
+              params: {
+                code,
+                client_id: FACEBOOK_CLIENT_ID,
+                client_secret: FACEBOOK_CLIENT_SECRET,
+                redirect_uri: 'http://localhost:8080/facebook'
+              }
+            }
+          )
+
+        const userId = await axios.get('https://graph.facebook.com/me', {
+            params: {
+                access_token: response.data.access_token
+            }
+        })
+
+        const userInfo = await axios.get(`https://graph.facebook.com/${userId.data.id}`, {
+            params: {
+                access_token: response.data.access_token,
+                fields: "id,name,email"
+            }
+        })
+
+        console.log(userInfo.data);
+
+    } catch (error) {
+        console.log(error.message);
+    }
+
+    res.redirect('http://localhost:3000/')
 })
 
 async function main() {  
